cad864c93dd31ceeb5eff2ae9f76a367fd01f92d
Londysiba <londekamhlengi.dlamini19@gmail.com>
2024-03-25 14:07:17 +0200

    Application javascript

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather defaultCity="Cape Town" />
+
+        <footer>
+          
+          <a
+            href="https://github.com/Londysiba/weather_react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub repository
+          </a>{" "}
+          Coded by Londeka Dlamini
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
